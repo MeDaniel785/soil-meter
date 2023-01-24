1bfc7eb633d143ef96ac2446fbe1d017cc9f0a3b
Daniel <66692561+MeDaniel785@users.noreply.github.com>
2023-01-24 21:02:46 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+    basic.showString("Compass calibrated.")
+    basic.showIcon(IconNames.Yes)
+})
+let reading = 0
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+basic.showIcon(IconNames.Happy)
+led.setBrightness(64)
+basic.forever(function () {
+    if (input.logoIsPressed()) {
+        basic.showString("The temperature is")
+        basic.showNumber(input.temperature())
+        basic.showString("°C")
+    }
+    if (input.isGesture(Gesture.Shake)) {
+        led.setBrightness(64)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        led.setBrightness(0)
+    }
+    reading = pins.analogReadPin(AnalogPin.P0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+        basic.pause(5000)
+    }
+})
